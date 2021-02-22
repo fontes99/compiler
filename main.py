d97111bb47e61ab9dd39b09798af1662c9477d1d
fontes99 <pvfontes1000@gmail.com>
2021-02-22 17:51:24 -0300

    almost done, dando erro com num > 99

---
+++ main.py
@@ -0,0 +1,36 @@
+import sys
+
+conta = sys.argv[1]
+
+res = 0
+
+nums = []
+op = []
+
+num = ''
+
+for i in conta:
+
+    if i == ' ':
+        pass
+    elif i !='+' and i != '-':
+        num += i
+    else:
+        op.append(i)
+        nums.append(int(num))
+        num = ''
+
+nums.append(int(num))
+
+res += nums[0]
+
+print(op)
+print(nums)
+
+for i in range(len(num)):
+    if op[i] == '+':
+        res += nums[i+1]
+    if op[i] == '-':
+        res -= nums[i+1]
+
+print(res)
