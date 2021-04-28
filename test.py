3d00e8897fca28727684c1289e40f8211893d03f
fontes99 <pvfontes1000@gmail.com>
2021-04-27 23:06:23 -0300

    minor fix

--- test.py
+++ test.py
@@ -1,16 +1,6 @@
 import subprocess
 import pytest
 
-# test geral
-def test_geral():
-    with open('conta.c', 'w') as f:
-        f.write(''' 
-                    x /*asdasda*/ = 3;
-                    y = 4;
-                    z = x + y + 100;
-                    println(x + y /*asdasda*/ + z); 
-                ''')
-    assert int(subprocess.check_output("python3 main.py conta.c", shell=True)) == 114
 
 def test_soma_simples():
     with open('conta.c', 'w') as f:
@@ -19,6 +9,7 @@ def test_soma_simples():
                     println(x); 
                 ''')
     assert int(subprocess.check_output("python3 main.py conta.c", shell=True)) == 3
+
 
 def test_soma_varias():
     with open('conta.c', 'w') as f:
@@ -30,3 +21,14 @@ def test_soma_varias():
                 ''')
     assert int(subprocess.check_output("python3 main.py conta.c", shell=True)) == 8
 
+
+# test geral
+def test_geral():
+    with open('conta.c', 'w') as f:
+        f.write(''' 
+                    x /*asdasda*/ = 3;
+                    y = 4;
+                    z = x + y + 100;
+                    println(x + y /*asdasda*/ + z); 
+                ''')
+    assert int(subprocess.check_output("python3 main.py conta.c", shell=True)) == 114
